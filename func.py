ae862740d70bf3c2ff3b37ba1c1aa467f0788197
Johan RAFANOMEZANTSOA (DSI) <ST121@ravinala-airports.aero>
2024-09-26 07:56:48 +0300

    finished configuring the OCR modul

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+from pdf2image import convert_from_path
 import pytesseract
 from PIL import Image
 import re
@@ -20,8 +21,8 @@ def extract_details(image_path):
     # for  ligne in lignes:
     #     print(ligne)
 
-    # Afficher les lignes qui contiennent "Ar" ou "4r" ou "A4r"
-    lignes_avec_ar = [ligne for ligne in lignes if 'Ar' in ligne or '4r' in ligne or 'A4r' in ligne or 'Ae' in ligne]
+    ##### Afficher les lignes qui contiennent "Ar" ou "4r" ou "A4r"
+    lignes_avec_ar = [ligne for ligne in lignes if 'Ar' in ligne or '4r' in ligne or 'A4r' in ligne or 'Ae' in ligne or 'br' in ligne or 'he' in ligne or 'ay' in ligne or 'hr' in ligne or 'AP' in ligne or 'A' in ligne or 'fr' in ligne or 'ar' in ligne or 'At' in ligne or 'PF' in ligne or 'r' in ligne]
     print(f"La longueur des mots est de {len(lignes_avec_ar)}")
 
     for line in lignes_avec_ar:
@@ -31,21 +32,31 @@ def extract_details(image_path):
 
     # Parcourir les lignes pour extraire les mots devant les motifs
     for ligne in lignes_avec_ar:
-        match = re.search(r'(\S+)\s*(?:/| ,00|, 00| a0| NA)', ligne)
+        match = re.search(r'(\S+)\s*(?:/| ,|,| a0| NA|000)', ligne)
         if match:
             mots_extraire.append(match.group(1))  # Ajouter le mot trouvé
 
-    print(f'Longueur = {len(mots_extraire)}')
+    print(f'----------- Longueur = {len(mots_extraire)}')
 
-    return mots_extraire
+    for line in mots_extraire:
+        print(line)
+
+
+
+
+
+
 
 
 
 # fonction qui extrait les mots d'une image
-def extract_text_from_image(image: Image.Image):
+def extract_text_from_image(image_path):
 
     # specification du chemin vers l'executable
     pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\st121\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
+
+    # charger l'image
+    image = Image.open(image_path)
 
     # Extraire le texte
     texte = pytesseract.image_to_string(image)
@@ -54,14 +65,13 @@ def extract_text_from_image(image: Image.Image):
     lignes = texte.splitlines()
 
     # Afficher les lignes qui contiennent "Ar" ou "4r" ou "A4r"
-    lignes_avec_ar = [ligne for ligne in lignes if 'Ar' in ligne or '4r' in ligne or 'A4r' in ligne or 'Ae' in ligne]
+    lignes_avec_ar = [ligne for ligne in lignes if 'Ar' in ligne or '4r' in ligne or 'A4r' in ligne or 'Ae' in ligne or 'br' in ligne or 'he' in ligne or 'ay' in ligne or 'hr' in ligne or 'AP' in ligne or 'A' in ligne or 'fr' in ligne or 'ar' in ligne or 'At' in ligne or 'PF' in ligne or 'r' in ligne]
 
-    # Initialiser une liste pour stocker les résultats
     mots_extraire = []
 
     # Parcourir les lignes pour extraire les mots devant les motifs
     for ligne in lignes_avec_ar:
-        match = re.search(r'(\S+)\s*(?:/| ,00|, 00| a0| NA)', ligne)
+        match = re.search(r'(\S+)\s*(?:/| ,|,| a0| NA|000)', ligne)
         if match:
             mots_extraire.append(match.group(1))  # Ajouter le mot trouvé
 
@@ -72,13 +82,18 @@ def extract_text_from_image(image: Image.Image):
 def get_prix_exact(mots):
     # Dictionnaire de correspondance entre préfixes et prix
     prix_mapping = {
-        '7': 700,
+        '7': 1500,
+        '16': 1500,
         '1': 1500,
+        't': 1500,
         'T': 1500,
-        '2': 2800,
-        '4': 4400,
+        '8': 2800,
+        '226': 2800,
         '22': 22500,
+        '2': 2800,
+        'M': 4400,
         '45': 45000,
+        '4': 4400,
         '0': 0
     }
     exact_price = []
